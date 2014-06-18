f0fac6a53046df1aafc3f2f70a2128a582e17120
Bruno Antônio de Pinho <bruno.antonio.pinho@gmail.com>
2014-06-18 17:09:59 -0300

    Flag
    Adicionado suportea a flag para não realizar a transmissão de pacotes

---
+++ arm/_build/main_tx.d
@@ -0,0 +1,17 @@
+.\_build\main_tx.o: ..\main_tx.c
+.\_build\main_tx.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\_build\main_tx.o: ..\radio_config.h
+.\_build\main_tx.o: ..\..\..\..\Include\nrf.h
+.\_build\main_tx.o: ..\..\..\..\Include\compiler_abstraction.h
+.\_build\main_tx.o: ..\..\..\..\Include\nrf51.h
+.\_build\main_tx.o: C:\Keil\ARM\CMSIS\Include\core_cm0.h
+.\_build\main_tx.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+.\_build\main_tx.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+.\_build\main_tx.o: ..\..\..\..\Include\system_nrf51.h
+.\_build\main_tx.o: ..\..\..\..\Include\nrf51_bitfields.h
+.\_build\main_tx.o: C:\Keil\ARM\CMSIS\Include\core_cm0.h
+.\_build\main_tx.o: ..\..\..\..\Include\nrf51_deprecated.h
+.\_build\main_tx.o: ..\..\..\..\Include\simple_uart.h
+.\_build\main_tx.o: C:\Keil\ARM\ARMCC\bin\..\include\stdbool.h
+.\_build\main_tx.o: ..\..\..\..\Include\boards.h
+.\_build\main_tx.o: ..\..\..\..\Include\boards/pca10000.h
